8c6ff1ef48ed0d250d9ed73eaf732072e09ebb94
madgic <olejikqq@gmail.com>
2024-10-06 20:01:02 +0300

    Edits for the first task

--- task-9/9/src/task1/Task1.java
+++ task-9/9/src/task1/Task1.java
@@ -1,33 +1,49 @@
 package task1;
 
 class Task1 extends Thread {
+    private final Object lock = new Object();
+
     @Override
     public void run() {
         try {
-            System.out.println("State: " + getState());
+            System.out.println("State (RUNNABLE): " + getState());
             Thread.sleep(100);
 
-            synchronized (this) {
-                System.out.println("State: " + getState());
-                wait(200);
+            synchronized (lock) {
+                System.out.println("State (RUNNABLE): " + getState());
+                System.out.println("State (WAITING): " + getState());
+                lock.wait();
             }
 
-            System.out.println("State: " + getState());
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            System.out.println("Thread interrupted.");
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println("=== Task 1 ===");
+
         Task1 task1 = new Task1();
-        System.out.println("Before start: " + task1.getState());
+        System.out.println("Before start (NEW): " + task1.getState());
+
         task1.start();
 
         Thread.sleep(50);
-        System.out.println("After start: " + task1.getState());
+        System.out.println("After start (RUNNABLE): " + task1.getState());
+
+        synchronized (task1.lock) {
+            Thread.sleep(50);
+            System.out.println("While holding lock (BLOCKED): " + task1.getState());
+        }
+
+        Thread.sleep(200);
+        System.out.println("In waiting state (WAITING): " + task1.getState());
+
+        synchronized (task1.lock) {
+            task1.lock.notify();
+        }
 
         task1.join();
-        System.out.println("After completion: " + task1.getState());
+        System.out.println("After completion (TERMINATED): " + task1.getState());
     }
 }
