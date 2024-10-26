b90b67aa0a223b016370a13cd3ae2e5d470c9a19
madgic <olejikqq@gmail.com>
2024-10-26 15:53:47 +0300

    Add DataBaseConnect

--- task-11/Code/src/main/java/autoservice/dataBase/connection/DatabaseConnection.java
+++ task-11/Code/src/main/java/autoservice/dataBase/connection/DatabaseConnection.java
@@ -1,10 +1,33 @@
 package autoservice.dataBase.connection;
 
 import java.sql.Connection;
-
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static Connection connection;
+    private static DatabaseConnection instance;
+    private final Connection connection;
+    private static final String URL = "jdbc:postgresql://localhost:5432/mydatabase";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "root";
 
 
+    private DatabaseConnection() {
+        try {
+            this.connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static DatabaseConnection getInstance() throws SQLException {
+        if (instance == null ||instance.getConnection().isClosed()) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 }
