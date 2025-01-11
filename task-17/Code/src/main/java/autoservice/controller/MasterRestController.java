6c7ae8c09551e7f9a34afeb909e85a47db8c3d5b
Olejik <134807586+Mad-Irishman@users.noreply.github.com>
2025-01-11 13:21:36 +0300

    some change controller

--- task-17/Code/src/main/java/autoservice/controller/MasterRestController.java
+++ task-17/Code/src/main/java/autoservice/controller/MasterRestController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/masters")
+@RequestMapping(path = "/admin/masters")
 public class MasterRestController {
 
     private final ServiceManager serviceManager;
